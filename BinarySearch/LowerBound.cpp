4fb010f4fa57a9eb68e24426252545e50347c13c
Tanvir <u1604090@student.cuet.ac.bd>
2023-08-17 01:02:23 +0600

    Lower Bound

---
+++ BinarySearch/LowerBound.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll            long long
+#define PI            acos(-1)
+#define RESET(a,b)    memset(a,b,sizeof(a))
+#define maxi          INT_MAX
+#define mini          INT_MIN
+
+int LowerBound(int ara[],int startIndx,int endIndx,int value){
+    if(startIndx<endIndx){
+        int mid = (startIndx+endIndx)/2;
+        if(ara[mid]>=value)return LowerBound(ara,startIndx,mid,value);
+        else return LowerBound(ara,mid+1,endIndx,value);
+    }
+    return startIndx;
+}
+
+int main() {
+
+    int ara[]={1,2,3,4,5,6,7,8,9,10,10,12,13,14,15,16};
+    int TC;
+    cin >> TC;
+    while (TC--) {
+        int searchValue;
+        cout<<"Enter your desired value: ";
+        cin>>searchValue;
+        cout<<"Lower Bound of the "<<searchValue<<" is :"<<LowerBound(ara,0,15,searchValue)<<endl;
+    }
+    return 0;
+}
