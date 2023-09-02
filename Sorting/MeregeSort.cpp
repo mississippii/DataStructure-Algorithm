7d8bc438e5012134afdd13b5fe6ea3611c3920a2
Tanvir <u1604090@student.cuet.ac.bd>
2023-09-03 01:44:14 +0600

    Merge sort

--- Sorting/MeregeSort.cpp
+++ Sorting/MeregeSort.cpp
@@ -8,7 +8,8 @@ using namespace std;
 #define mini          INT_MIN
 
 //Time Complexity O(n log(n))
-int temp[10000]; //n is the maximum size of an array
+
+int temp[10000];
 
 void  mergeSort(int Ara[],int startingIndex,int endingIndex){
 
@@ -20,6 +21,7 @@ void  mergeSort(int Ara[],int startingIndex,int endingIndex){
     mergeSort(Ara,mid+1,endingIndex);
 
     for(int i=startingIndex,j=mid+1,k=startingIndex;k<=endingIndex;k++){
+
         if(i==mid+1)temp[k]=Ara[j++];
         else if (j==endingIndex+1)temp[k]=Ara[i++];
         else if (Ara[i]<Ara[j])temp[k]=Ara[i++];
