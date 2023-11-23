6fef107b571fe9b0b4e904611ab73ec702f3fed7
Tanvir <u1604090@student.cuet.ac.bd>
2023-11-23 21:43:44 +0600

    BFS Algorithm Implemented

---
+++ Algorithm/BreadthFirstSearch.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll            long long
+#define PI            acos(-1)
+#define RESET(a,b)    memset(a,b,sizeof(a))
+#define maxi          INT_MAX
+#define mini          INT_MIN
+
+void TraversWithBFS(vector<vector<int>>& graph, vector<bool>& visited, int src){
+    queue<int>Q;
+    Q.push(src);
+    while(!Q.empty()) {
+        int temp = Q.front();
+        cout<<temp<<"-->";
+        Q.pop();
+        for(auto x : graph[temp]){
+            if(!visited[x]){
+            visited[x] = true;
+            Q.push(x);
+            }
+        }
+    }
+    cout<<endl;
+    return;
+
+}
+
+int main() {
+
+    vector<vector<int>>Graph ={
+        {},
+        {2,3,4},
+        {6},{7,8},
+        {7},
+        {8,10},
+        {9},
+        {3,4,9},
+        {3,5},
+        {6,7,10},
+        {9,5}};
+        
+   int src=1;
+   vector<bool>visited(11);
+   TraversWithBFS(Graph,visited,src);
+    return 0;
+}
+
