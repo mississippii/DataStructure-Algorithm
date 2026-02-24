7ad6a65cb6e9dfa24127ebed1a08525445492d4c
T@nvir <veer.cuet@gmail.com>
2026-02-25 03:43:53 +0600

    modification

---
+++ Recursion/RecursionBasic.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long
+#define PI            acos(-1)
+#define RESET(a,b)    memset(a,b,sizeof(a))
+#define maxi          INT_MAX
+#define mini          INT_MIN
+
+void headRecursion(int n) {
+    if (n == 0) return; // Base case
+
+    headRecursion(n - 1); // Recursive call
+
+    cout << n << " "; // Process after recursive call
+}
+void tailRecursion(int n) {
+    if (n == 0) return; // Base case
+    cout << n << " "; // Process after recursive call
+    tailRecursion(n - 1); // Recursive call
+    
+}
+
+int main() {
+    cout << "=== Recursion Basics ===" << endl;
+
+    // Example: Factorial of a number
+    int n = 5;
+    cout << "Head Recursion of " << n << " is: ";
+    headRecursion(n);
+    cout << endl;
+    cout << "Tail Recursion of " << n << " is: ";
+    tailRecursion(n);
+    cout << endl;
+
+    return 0;
+}
